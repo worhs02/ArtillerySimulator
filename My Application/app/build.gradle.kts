7ca730ae294ea8297329a978efd2b9d1803dd721
Azin <setiguy@gachon.ac.kr>
2024-03-12 19:02:51 +0900

    map preview

--- My Application/app/build.gradle.kts
+++ My Application/app/build.gradle.kts
@@ -47,6 +47,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.naver.maps:map-sdk:3.14.0")
+    implementation ("com.naver.maps:map-sdk:3.17.0")
 
 }
